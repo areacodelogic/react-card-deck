8b77b0886b76598914bfc52c2469fdb6d740883f
Luke Nemy <lukenemy7@gmail.com>
2019-09-23 11:42:42 -0700

    added cards css, still needs to fix distracting render re-positioning

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,17 @@ import "./cards.css";
 
 class Card extends Component {
   render() {
+
+    // will turn every card -> everytime because these random values are generated everytime the page renders
+    //fix will be using some type of state when drawing a card and setting these values at that point in time, then sending them in as props to this Card component
+    let degree = Math.floor(Math.random() * 90) - 45;
+    let scootVert = Math.floor(Math.random() * 20);
+    let scootHorz = Math.floor(Math.random() * 20);
     return (
       <div className="card">
-        <img id={this.props.id} src={this.props.image}></img>
+        <img id={this.props.id} src={this.props.image}
+        style={ {transform: `rotate(${degree}deg) translate(${scootHorz}px,${scootVert}px)`}}
+        alt={this.props.id}></img>
       </div>
     );
   }
